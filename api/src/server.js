f213f06b3dce56f60072558f32f9e9dd4e59363f
badmood111 <petrzela.adam@seznam.cz>
2023-10-12 12:20:12 +0200

    init

--- api/src/server.js
+++ api/src/server.js
@@ -1,17 +1,10 @@
-const { ApolloServer } = require('apollo-server')
-const typeDefs = require('./schema')
-const resolvers = require('./resolvers')
-const {models, db} = require('./db')
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const { models, db } = require("./db");
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context() {
-    const user = db.get('user').value()
-    return {models, db, user}
-  }
-})
+const server = new ApolloServer();
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
-})
+});
