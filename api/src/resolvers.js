a229de0bd10ff1ec33b164665207afb3c10c8273
badmood111 <petrzela.adam@seznam.cz>
2023-10-18 14:51:01 +0200

    add pet / pets queries & qruey resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,14 +1,24 @@
 module.exports = {
   Query: {
+    pets(_, { input }, context) {
+      return context.models.Pet.findMany(input); //findMany takes in an input - object of fields that you want to filer on
+    },
 
+    pet(_, { input }, context) {
+      return context.models.Pet.findOne(input);
+    },
   },
-  Mutation: {
-    
-  },
+  // Mutation: {},
   Pet: {
-    
+    id(pet) {
+      console.log(pet);
+      return pet.id;
+    },
+    // img(pet) {
+    //   return pet.type === "DOG"
+    //     ? "https://placedog.net/300/300"
+    //     : "http://placekitten.com/300/300";
+    // },
   },
-  User: {
-    
-  }
-}
+  // User: {},
+};
--- api/src/schema.js
+++ api/src/schema.js
@@ -1,7 +1,27 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
+
+  input PetInput {
+    name: String
+    type: String
+  }
+ 
+  type Query {
+    pet(input: PetInput): Pet!
+    pets(input: PetInput): [Pet]!
+  }
 `;
 
 module.exports = typeDefs;
